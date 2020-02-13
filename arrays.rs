b0692f77dbc0d0ce282f9f8563dd47dbbc75b2ce
Liberbell <liberbell@gmail.com>
2020-02-14 07:34:58 +0900

    arrays2

--- arrays.rs
+++ arrays.rs
@@ -1,6 +1,6 @@
 fn main() {
     let months = ["January", "February", "March", "April", "May", "Jun", "July", "August",
-    "September", "October", "November", "December"]
+    "September", "October", "November", "December"];
 
-    println!("First Month = {}", months[0])
+    println!("First Month = {}", months[0]);
 }
