652f9f5e9c8161755371279eee6d13694e3457c0
Liberbell <liberbell@gmail.com>
2020-02-15 21:57:11 +0900

    main5

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let a = 15;
+    let a = 6;
 
     if a < 10 {
         println!("Condition is true")
     } else {
         println!("Condition is false")
+    }
+
+    if a % 4 == 0 {
+        println!("A is divisible by 4")
+    } else if a % 3 == 0 {
+        println!("A is divisible by 3")
+    } else if a % 2 == 0 {
+        println!("A is disviible by 2")
     }
 }
