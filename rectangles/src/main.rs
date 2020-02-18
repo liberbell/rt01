c81cec9de0abccc44e5fc77f0a25d3cc847c8eb3
Liberbell <liberbell@gmail.com>
2020-02-19 07:37:27 +0900

    main4

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,10 +1,21 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let width = 30;
     let height = 50;
+
+    let rect1 = Rectangle {width: 10, height:30};
 
     println!("Area is {}", area(width, height));
 }
 
 fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area2(width: u32, height: u32) -> u32 {
     width * height
 }
