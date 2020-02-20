5047bd4d1a9ce55852cd9ffeb4436eb37dc534a6
Liberbell <liberbell@gmail.com>
2020-02-21 07:02:30 +0900

    patterns4

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let name = String::from("ram");
 
-    println!("character at position 4 is {}", match name.chars().nth(4) {
+    println!("character at position 2 is: {}", match name.chars().nth(2) {
         Some(c) => c.to_string(),
         None => "No character found".to_string(),
     }
