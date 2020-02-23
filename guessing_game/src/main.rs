81d6d87d6127f2f060aa6caed9714804e6b651fa
Liberbell <liberbell@gmail.com>
2020-02-23 10:57:05 +0900

    guessing_game3

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,4 +2,8 @@ use std::io
 
 fn main() {
     println!("Please input your guess...");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("failed to read line.")
 }
