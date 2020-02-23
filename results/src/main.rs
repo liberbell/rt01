6fa192f5539b348ec93096ebf7881152744e3479
Liberbell <liberbell@gmail.com>
2020-02-23 10:38:33 +0900

    results1

--- results/src/main.rs
+++ results/src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 fn main() {
     let f = File::open("hello.txt");
 
-    let foo = match f {
+    let _foo = match f {
         Ok(file) => file,
         Err(error) => {
             panic!("File was not found");
