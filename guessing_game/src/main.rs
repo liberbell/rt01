d3e112039dcb72cb454bcc6848254ce28c57dfc9
Liberbell <liberbell@gmail.com>
2020-02-24 16:50:28 +0900

    guessing_game6

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,6 +5,8 @@ use rand::Rng;
 fn main() {
     println!("Please input your guess...");
 
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
         .expect("failed to read line.");
