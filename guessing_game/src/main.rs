9d8786f60240644cb02a43200b0bbbd71c59ab16
Liberbell <liberbell@gmail.com>
2020-02-24 16:57:52 +0900

    guessing_game9

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,16 @@
 extern crate rand;
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Please input your guess...");
+
+    println!("Welcome to the guessing game.")
 
     let secret_number = rand::thread_rng().gen_range(1,101);
     println!("Secret number is = {}", secret_number);
+
+    println!("Please input your guess...");
 
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
