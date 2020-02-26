625b5bbc79ca1b5d9db76f05a6d4c9352101c5a2
eliberbell <liberbell@gmail.com>
2020-02-26 22:14:42 +0900

    Update main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,7 +6,8 @@ use std::cmp::Ordering;
 fn main() {
     //Start
     println!("Welcome to the guessing game.");
-
+　　
+    
     let secret_number = rand::thread_rng().gen_range(1,101);
     println!("Secret number is = {}", secret_number);
 
